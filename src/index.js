50136179bdabbd95fc4fdab91c5cb7ac9d5c5482
Wii Yang <wii_yang@163.com>
2024-07-24 14:55:10 +0800

    feat: 添加启动成功提示

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ const { exec } = require('child_process')
 const { wechatUrl } = require('../config/index')
 
 function main() {
-  exec(`"${wechatUrl}"`)
-  exec(`"${wechatUrl}"`)
+  console.log('微信启动中')
+  const process1 = exec(`"${wechatUrl}"`)
+  const process2 = exec(`"${wechatUrl}"`)
+  Promise.all([process1, process2]).then(() => {
+    console.log('微信启动完成')
+  }).catch(() => {
+    console.log('程序执行出错')
+  })
 }
 
 module.exports = main
